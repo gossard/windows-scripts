5afd8b04708cfb2d2790e35d0afd9d1d34a87e16
Florent Gossard <florentgossard@gmail.com>
2022-12-05 12:23:03 +0100

    Add files via upload

---
+++ Util/GenerateAESKey.ps1
@@ -0,0 +1,14 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({Test-Path $_ -PathType Container})]
+    [string]$Path,
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Name,
+    [Parameter(Mandatory=$true)]
+    [ValidateSet(16, 32)]
+    [int]$Size
+)
+[byte[]]$bytes = New-Object byte[] -ArgumentList $Size
+[System.Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($bytes)
+$bytes | Out-File (Join-Path -Path $Path -ChildPath $Name)
