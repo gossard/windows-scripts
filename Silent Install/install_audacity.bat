0d3d0239edc98ad5b06c892ed0c290f973710c09
Florent Gossard <florentgossard@gmail.com>
2023-01-04 11:11:14 +0100

    Update install_audacity.bat

--- Silent Install/install_audacity.bat
+++ Silent Install/install_audacity.bat
@@ -1,12 +1,13 @@
 @echo off
 
 REM Install Audacity if not already installed.
+REM Supported version: 3.2.2-x64
 REM author: Florent Gossard
 REM date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES%\Audacity\Audacity.exe" 
-set sharedExePath="\\Share$\Audacity\audacity-win-3.2.2-x64"
+set sharedExePath="\\Share$\Audacity\audacity"
 
 echo Installing Audacity... 
 if not exist %localExePath% %sharedExePath% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
