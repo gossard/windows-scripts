57903a7232c789e1c8a1af36d7ca766562a864ca
Florent Gossard <florentgossard@gmail.com>
2023-01-04 11:19:43 +0100

    Update install_npp.bat

--- Silent Install/install_npp.bat
+++ Silent Install/install_npp.bat
@@ -1,12 +1,13 @@
 @echo off
 
 REM Install NotePad++ if not already installed.
+REM Supported version: 8.3.3
 REM author: Florent Gossard
 REM date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES%\NotePad++\notepad++.exe"
-set sharedExePath="\\Share$\NotePad++\npp.8.3.3.Installer.x64.exe"
+set sharedExePath="\\Share$\NotePad++\npp.exe"
 
 echo Installing NotePad++... 
 if not exist %localExePath% %sharedExePath% /S
