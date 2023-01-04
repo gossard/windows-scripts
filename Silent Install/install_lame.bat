8906aab177102b81406ee5d91bf09c1cd179cfe3
Florent Gossard <florentgossard@gmail.com>
2023-01-04 11:19:10 +0100

    Update install_lame.bat

--- Silent Install/install_lame.bat
+++ Silent Install/install_lame.bat
@@ -1,12 +1,13 @@
 @echo off
 
 REM Install Lame if not already installed.
+REM Supported version: 3.99.3
 REM author: Florent Gossard
 REM date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES(X86)%\Lame for Audacity\lame.exe" 
-set sharedExePath="\\Share$\Lame\Lame_v3.99.3_for_Windows.exe"
+set sharedExePath="\\Share$\Lame\lame.exe"
 
 echo Installing Lame... 
 if not exist %localExePath% %sharedExePath% /VERYSILENT /NORESTART
