31552fc07c4c146da86d5e9b45f8cfa8c41be7b8
Florent Gossard <florentgossard@gmail.com>
2023-01-04 11:17:42 +0100

    Update install_gimp.bat

--- Silent Install/install_gimp.bat
+++ Silent Install/install_gimp.bat
@@ -1,12 +1,13 @@
 @echo off
 
 REM Install Gimp if not already installed.
+REM Supported version: 2.10.30
 REM author: Florent Gossard
 REM date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES%\GIMP 2\bin\gimp-2.10.exe" 
-set sharedExePath="\\Share$\Gimp\gimp-2.10.30-setup.exe"
+set sharedExePath="\\Share$\Gimp\gimp.exe"
 
 echo Installing Gimp... 
 if not exist %localExePath% %sharedExePath% /VERYSILENT /NORESTART /ALLUSERS
