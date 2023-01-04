7ffdaa7cb0bbf4432dbf88fc0504a3739e187008
Florent Gossard <florentgossard@gmail.com>
2023-01-04 11:20:40 +0100

    Update install_vlc.bat

--- Silent Install/install_vlc.bat
+++ Silent Install/install_vlc.bat
@@ -1,12 +1,13 @@
 @echo off
 
 REM Install VLC if not already installed.
+REM Supported version: 3.0.16(x64)
 REM author: Florent Gossard
 REM date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES%\VideoLAN\VLC\vlc.exe"
-set sharedExePath="\\Share$\VLC\vlc-3.0.16-win64.exe"
+set sharedExePath="\\Share$\VLC\vlc.exe"
 
 echo Installing VLC... 
 if not exist %localExePath% %sharedExePath% /S
