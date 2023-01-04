76e4fb931eb0c1f83a2b05f9c8df872d1a45a067
Florent Gossard <florentgossard@gmail.com>
2023-01-04 11:16:31 +0100

    Update install_geogebra.bat

--- Silent Install/install_geogebra.bat
+++ Silent Install/install_geogebra.bat
@@ -1,12 +1,13 @@
 @echo off
 
 REM Install GeoGebra if not already installed.
+REM Supported version: 5.0.700.0
 REM author: Florent Gossard
 REM date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES(X86)%\GeoGebra 5.0\GeoGebra.exe" 
-set sharedMsiPath="\\Share$\GeoGebra\GeoGebra-Windows-Installer-5-0-700-0.msi"
+set sharedMsiPath="\\Share$\GeoGebra\geogebra.msi"
 
 echo Installing GeoGebra... 
 if not exist %localExePath% msiexec /i %sharedMsiPath% ALLUSERS=2 /qn
