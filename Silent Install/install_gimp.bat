c1bc671ea3715cf7afe6c88dc52535c1ead5a3f0
Florent Gossard <florentgossard@gmail.com>
2023-01-06 09:52:51 +0100

    Update install_gimp.bat

--- Silent Install/install_gimp.bat
+++ Silent Install/install_gimp.bat
@@ -2,8 +2,8 @@
 
 REM Install Gimp if not already installed.
 REM Supported version: 2.10.30
-REM author: Florent Gossard
-REM date: 12/12/2022
+REM Author: Florent Gossard
+REM Date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES%\GIMP 2\bin\gimp-2.10.exe" 
