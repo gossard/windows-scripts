d3207669d78eb618b39bc9e3ece8ac424d6213df
Florent Gossard <florentgossard@gmail.com>
2023-01-06 10:01:55 +0100

    Update install_ganttproject.bat

--- Silent Install/install_ganttproject.bat
+++ Silent Install/install_ganttproject.bat
@@ -7,7 +7,7 @@ REM Date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES(X86)%\GanttProject-3.2\ganttproject.exe"
-set sharedExePath="\\Share$\GanttProject\ganttproject.exe"
+set sharedExePath="\\SRV\Share$\GanttProject\ganttproject.exe"
 
 echo Installing GanttProject...
 if not exist %localExePath% %sharedExePath% /S
