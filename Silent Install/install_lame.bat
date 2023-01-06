30f7aebf35a3416129f02a93cc5fdc9bc66ba6b7
Florent Gossard <florentgossard@gmail.com>
2023-01-06 10:02:59 +0100

    Update install_lame.bat

--- Silent Install/install_lame.bat
+++ Silent Install/install_lame.bat
@@ -7,7 +7,7 @@ REM Date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES(X86)%\Lame for Audacity\lame.exe" 
-set sharedExePath="\\Share$\Lame\lame.exe"
+set sharedExePath="\\SRV\Share$\Lame\lame.exe"
 
 echo Installing Lame... 
 if not exist %localExePath% %sharedExePath% /VERYSILENT /NORESTART
