dbdd5682e29ba40d679344be23c4e074919abf4d
Florent Gossard <florentgossard@gmail.com>
2023-01-06 09:53:37 +0100

    Update install_vlc.bat

--- Silent Install/install_vlc.bat
+++ Silent Install/install_vlc.bat
@@ -2,8 +2,8 @@
 
 REM Install VLC if not already installed.
 REM Supported version: 3.0.16
-REM author: Florent Gossard
-REM date: 12/12/2022
+REM Author: Florent Gossard
+REM Date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES%\VideoLAN\VLC\vlc.exe"
