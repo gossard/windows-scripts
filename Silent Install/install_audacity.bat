d64c354e18d74516515d360a4bdb06c0dfc4d54e
Florent Gossard <florentgossard@gmail.com>
2023-01-06 10:01:35 +0100

    Update install_audacity.bat

--- Silent Install/install_audacity.bat
+++ Silent Install/install_audacity.bat
@@ -7,7 +7,7 @@ REM Date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES%\Audacity\Audacity.exe" 
-set sharedExePath="\\Share$\Audacity\audacity.exe"
+set sharedExePath="\\SRV\Share$\Audacity\audacity.exe"
 
 echo Installing Audacity... 
 if not exist %localExePath% %sharedExePath% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
