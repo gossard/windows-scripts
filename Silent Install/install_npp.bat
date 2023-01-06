0afdfd53ae40a4a80ab9e8ed1e0628e8f94c7a13
Florent Gossard <florentgossard@gmail.com>
2023-01-06 09:53:21 +0100

    Update install_npp.bat

--- Silent Install/install_npp.bat
+++ Silent Install/install_npp.bat
@@ -2,8 +2,8 @@
 
 REM Install NotePad++ if not already installed.
 REM Supported version: 8.3.3
-REM author: Florent Gossard
-REM date: 12/12/2022
+REM Author: Florent Gossard
+REM Date: 12/12/2022
 
 REM Change these variables to match your configuration.
 set localExePath="%PROGRAMFILES%\NotePad++\notepad++.exe"
